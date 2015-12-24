4e953d44cfdc3e7f1316523f2151540dc1539aa6
Kerem Gunes <qeremy@gmail.com>
2015-12-25 00:36:43 +0200

    add GraphCommons\Graph\Entity\Layout object.

---
+++ GraphCommons/Graph/Entity/Layout.php
@@ -0,0 +1,87 @@
+<?php
+namespace GraphCommons\Graph\Entity;
+
+use GraphCommons\Graph\GraphEntity;
+
+final class Layout extends GraphEntity
+{
+    private $springLength;
+    private $gravity;
+    private $springCoeff;
+    private $dragCoeff;
+    private $theta;
+    private $algorithm;
+    private $transform;
+
+    final public function setSpringLength(int $springLength): self
+    {
+        $this->springLength = $springLength;
+        return $this;
+    }
+    final public function setGravity(float $gravity): self
+    {
+        $this->gravity = $gravity;
+        return $this;
+    }
+    final public function setGpringCoeff(float $springCoeff): self
+    {
+        $this->springCoeff = $springCoeff;
+        return $this;
+    }
+    final public function setGragCoeff(float $dragCoeff): self
+    {
+        $this->dragCoeff = $dragCoeff;
+        return $this;
+    }
+    final public function setGheta(float $theta): self
+    {
+        $this->theta = $theta;
+        return $this;
+    }
+    final public function setAlgorithm(string $algorithm): self
+    {
+        $this->algorithm = $algorithm;
+        return $this;
+    }
+    final public function setTransform(string $transform): self
+    {
+        $this->transform = $transform;
+        return $this;
+    }
+
+    final public function getSpringLength(): int
+    {
+        $this->springLength = $springLength;
+        return $this;
+    }
+    final public function getGravity(): float
+    {
+        $this->gravity = $gravity;
+        return $this;
+    }
+    final public function getGpringCoeff(): float
+    {
+        $this->springCoeff = $springCoeff;
+        return $this;
+    }
+    final public function getGragCoeff(): float
+    {
+        $this->dragCoeff = $dragCoeff;
+        return $this;
+    }
+    final public function getGheta(): float
+    {
+        $this->theta = $theta;
+        return $this;
+    }
+    final public function getAlgorithm(): string
+    {
+        $this->algorithm = $algorithm;
+        return $this;
+    }
+    final public function getTransform(): string
+    {
+        $this->transform = $transform;
+        return $this;
+    }
+}
