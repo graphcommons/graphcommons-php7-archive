793a4bb5e2b0ff6bc78c06a3aa849c8e29f83db0
Kerem Gunes <qeremy@gmail.com>
2015-12-25 01:43:47 +0200

    remove unneded methods, set $apiKey as required.

--- GraphCommons/GraphCommons.php
+++ GraphCommons/GraphCommons.php
@@ -8,21 +8,9 @@ final class GraphCommons
     private $client;
     private $apiKey;
 
-    final public function __construct(string $apiKey = '', array $config = []) {
-        $client = new Client();
-        $this->setClient($client);
-        $this->setApiKey($apiKey);
-    }
-
-    final public function setClient(Client $client): self
-    {
-        $this->client = $client;
-        return $this;
-    }
-    final public function setApiKey(string $apiKey): self
-    {
+    final public function __construct(string $apiKey, array $config = []) {
+        $this->client = new Client();
         $this->apiKey = trim($apiKey);
-        return $this;
     }
 
     final public function getClient(): Client
