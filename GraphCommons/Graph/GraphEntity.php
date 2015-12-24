44327a5f7f0ae2d2eab739d9216dcad0c13c54b8
Kerem Gunes <qeremy@gmail.com>
2015-12-25 00:34:38 +0200

    add GraphCommons\Graph\GraphEntity object.

---
+++ GraphCommons/Graph/GraphEntity.php
@@ -0,0 +1,21 @@
+<?php
+namespace GraphCommons\Graph;
+
+final class GraphEntity
+{
+    protected $graph;
+
+    final public function __construct(Graph $graph = null) {
+        $this->graph = $graph;
+    }
+
+    final public function setGraph(Graph $graph): self
+    {
+        $this->graph = $graph;
+        return $this;
+    }
+    final public function getGraph(): Graph
+    {
+        return $this->graph;
+    }
+}
