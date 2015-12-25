dbb71098ba1f86b84867a970ad3bee2a74486993
Kerem Gunes <qeremy@gmail.com>
2015-12-25 16:43:43 +0200

    remove final cos it will extended.

--- GraphCommons/GraphCommonsApiException.php
+++ GraphCommons/GraphCommonsApiException.php
@@ -1,7 +1,7 @@
 <?php
 namespace GraphCommons;
 
-final class GraphCommonsApiException extends \Exception
+class GraphCommonsApiException extends \Exception
 {
     const UNKNOWN_ERROR_CODE = 0;
     const UNKNOWN_ERROR_MESSAGE = 'Unknown error!';
