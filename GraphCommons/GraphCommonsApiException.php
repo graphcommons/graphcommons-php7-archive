794ba6602677afb048c4fd4cb1f900e97a771f45
Kerem Gunes <qeremy@gmail.com>
2015-12-25 05:18:54 +0200

    change const name.

--- GraphCommons/GraphCommonsApiException.php
+++ GraphCommons/GraphCommonsApiException.php
@@ -4,5 +4,5 @@ namespace GraphCommons;
 final class GraphCommonsApiException extends \Exception
 {
     const UNKNOWN_ERROR_CODE = 0;
-    const UNKNOWN_ERROR_TEXT = 'Unknown error!';
+    const UNKNOWN_ERROR_MESSAGE = 'Unknown error!';
 }
