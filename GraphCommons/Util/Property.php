aafb8800e0082c6195622cce076036597347b641
Kerem Gunes <qeremy@gmail.com>
2015-12-25 05:39:15 +0200

    add setReadonly(), getReadonly() methods.

--- GraphCommons/Util/Property.php
+++ GraphCommons/Util/Property.php
@@ -3,10 +3,19 @@ namespace GraphCommons\Util;
 
 trait Property
 {
-    private $readOnly = true;
+    private $readonly = true;
+
+    public function setReadonly(bool $readonly)
+    {
+        $this->readonly = $readonly;
+    }
+    public function getReadonly(): bool
+    {
+        return $this->readonly;
+    }
 
     public function __set(string $name, $value) {
-        if ($this->readOnly) {
+        if ($this->readonly) {
             throw new \Exception(sprintf('%s object is readonly!',
                 get_class($this)));
         }
