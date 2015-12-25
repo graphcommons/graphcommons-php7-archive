edec573f90a21564a09c90afb079560670fdc886
Kerem Gunes <qeremy@gmail.com>
2015-12-26 00:14:42 +0200

    signal tests.

--- test/test.php
+++ test/test.php
@@ -5,14 +5,23 @@ define('API_KEY', 'sk_Bb9mIS0oQy8XBS8OX0ZHNg');
 $autoload = require('../GraphCommons/Autoload.php');
 $autoload->register();
 
-$gc = new GraphCommons\GraphCommons(API_KEY);
+use GraphCommons\GraphCommons;
+use GraphCommons\Graph\Signal;
+use GraphCommons\Graph\Signals;
+
+$gc = new GraphCommons(API_KEY);
 // pre($gc);
 
 // check status
 // $data = $gc->api->status();
 
 // get graph
-$data = $gc->api->getGraph('8f8c794a-a498-4c3e-a73b-95a460db6e3a');
+// $data = $gc->api->getGraph('8f8c794a-a498-4c3e-a73b-95a460db6e3a');
 
 // dump data
-prd($data);
+// pre($data);
+
+$ss = new Signals();
+$ss->add(new Signal(Signal::NODE_CREATE));
+$ss->add(new Signal(Signal::NODE_UPDATE));
+pre($ss);
