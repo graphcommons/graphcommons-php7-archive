07103b0e6d1b51f8ea642ac29b4ce7e9630ec81f
Kerem Gunes <qeremy@gmail.com>
2015-12-25 17:01:18 +0200

    update error message.

--- GraphCommons/GraphCommonsApiException.php
+++ GraphCommons/GraphCommonsApiException.php
@@ -4,5 +4,5 @@ namespace GraphCommons;
 class GraphCommonsApiException extends \Exception
 {
     const UNKNOWN_ERROR_CODE = 0;
-    const UNKNOWN_ERROR_MESSAGE = 'Unknown error!';
+    const UNKNOWN_ERROR_MESSAGE = 'unknown error';
 }
