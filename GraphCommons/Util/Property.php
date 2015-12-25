0fb86b6119e22916c11d6953632a3a87004235d7
Kerem Gunes <qeremy@gmail.com>
2015-12-25 05:36:52 +0200

    final keyword useless in traits kerem!

--- GraphCommons/Util/Property.php
+++ GraphCommons/Util/Property.php
@@ -5,14 +5,14 @@ trait Property
 {
     private $readOnly = true;
 
-    final public function __set(string $name, $value) {
+    public function __set(string $name, $value) {
         if ($this->readOnly) {
             throw new \Exception(sprintf('%s object is readonly!',
                 get_class($this)));
         }
         $this->{$name} = $value;
     }
-    final public function __get(string $name) {
+    public function __get(string $name) {
         if (!property_exists($this, $name)) {
             throw new \Exception(sprintf('%s property is not exists on %s',
                 $name, get_class($this)));
