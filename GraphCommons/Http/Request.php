a6ba54fe7d5889346f20953bc473e907da4904ef
Kerem Gunes <qeremy@gmail.com>
2015-12-25 03:29:38 +0200

    add send() method.

--- GraphCommons/Http/Request.php
+++ GraphCommons/Http/Request.php
@@ -54,5 +54,67 @@ final class Request extends Stream
     {}
 
     final public function send()
-    {}
+    {
+        $url = parse_url($this->uri);
+        pre($this->uri);
+        pre($url);
+
+        $url['query'] = isset($url['query']) ? '?'. $url['query'] : '';
+
+        $sockProt = 'tcp://';
+        $sockPort = 80;
+        if ($url['scheme'] == 'https') {
+            $sockProt = 'ssl://';
+            $sockPort = 443;
+        }
+
+        $sock =@ fsockopen(
+            $sockProt . $url['host'],
+            $sockPort,
+            $this->failCode,
+            $this->failText,
+            $this->client->config['timeout_connection']
+        );
+        if (is_resource($sock)) {
+            $headers = array();
+            $headers['Host'] = sprintf('%s:%s', $url['host'], $sockPort);
+            $headers['Connection'] = 'close';
+            foreach ($this->headers as $key => $value) {
+                // actually remove header command
+                if ($value !== null) {
+                    $headers[$key] = $value;
+                }
+            }
+
+            fwrite($sock, sprintf("%s %s%s HTTP/%s\r\n",
+                $this->method, $url['path'], $url['query'], $this->httpVersion));
+            foreach ($headers as $key => $value) {
+                fwrite($sock, sprintf("%s: %s\r\n", $key, $value));
+            }
+            fwrite($sock, "\r\n");
+            fwrite($sock, $this->body);
+
+            stream_set_timeout($sock, $this->client->config['timeout_read']);
+            stream_set_blocking($sock, $this->client->config['blocking']);
+            $meta = stream_get_meta_data($sock);
+
+            $result = '';
+            while (!feof($sock)) {
+                // check timeout
+                if ($meta['timed_out']) {
+                    $this->failText = 'Timed out!';
+                    break;
+                }
+                $result .= fread($sock, 1024);
+                // get meta again
+                $meta = stream_get_meta_data($sock);
+            }
+
+            fclose($sock);
+
+            return $result;
+        }
+
+        return null;
+    }
 }
