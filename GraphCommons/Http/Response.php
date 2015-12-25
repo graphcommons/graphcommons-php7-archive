05032b4c311678bb8cb2a4f99fe2a543978f3442
Kerem Gunes <qeremy@gmail.com>
2015-12-25 04:59:10 +0200

    remove STATUSES const array, add 200 OK status.

--- GraphCommons/Http/Response.php
+++ GraphCommons/Http/Response.php
@@ -5,19 +5,8 @@ use GraphCommons\Http\Client;
 
 final class Response extends Stream
 {
-    const STATUSES = array(
-        400 => 'Bad Request',
-        401 => 'Unauthorized',
-        403 => 'Forbidden',
-        404 => 'Not Found',
-        405 => 'Method Not Allowed',
-        406 => 'Not Acceptable',
-        410 => 'Gone',
-        429 => 'Too Many Requests',
-        500 => 'Internal Server Error',
-        503 => 'Service Unavailable',
-    );
-    const STATUS_BAD_REQUEST = 400,
+    const STATUS_OK = 200,
+          STATUS_BAD_REQUEST = 400,
           STATUS_UNAUTHORIZED = 401,
           STATUS_FORBIDDEN = 403,
           STATUS_NOT_FOUND = 404,
