afc9286f347816e198ca4b7304ef5826204ddbf3
Kerem Gunes <qeremy@gmail.com>
2015-12-25 19:36:40 +0200

    tests..

--- test/test.php
+++ test/test.php
@@ -5,24 +5,14 @@ define('API_KEY', 'sk_Bb9mIS0oQy8XBS8OX0ZHNg');
 $autoload = require('../GraphCommons/Autoload.php');
 $autoload->register();
 
-use GraphCommons\GraphCommons;
-
-// $config = array(
-//     'api_url' => 'http://localhost/foo.json',
-//     'api_version' => '',
-// );
-// $gc = new GraphCommons(API_KEY, $config);
-
-$gc = new GraphCommons(API_KEY);
+$gc = new GraphCommons\GraphCommons(API_KEY);
 // pre($gc);
 
+// check status
 // $data = $gc->api->status();
 
-// 8f8c794a-a498-4c3e-a73b-95a460db6e3a
-$data = $gc->api->graph('8f8c794a-a498-4c3e-a73b-95a460db6e3a');
+// get graph
+$data = $gc->api->getGraph('8f8c794a-a498-4c3e-a73b-95a460db6e3a');
 
-pre($data);
-
-// $client = $gc->getClient();
-// $client->get('');
-// pre($client);
+// dump data
+prd($data);
