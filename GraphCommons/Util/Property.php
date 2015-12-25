8af356862efba36e9d410bee2788af3a286623af
Kerem Gunes <qeremy@gmail.com>
2015-12-25 20:43:54 +0200

    add __isset(), __unset() methods.

--- GraphCommons/Util/Property.php
+++ GraphCommons/Util/Property.php
@@ -14,18 +14,31 @@ trait Property
         return $this->_readonly;
     }
 
-    public function __set(string $name, $value) {
+    public function __set(string $name, $value)
+    {
         if ($this->_readonly) {
             throw new \Exception(sprintf('%s object is readonly!',
                 get_class($this)));
         }
         $this->{$name} = $value;
     }
-    public function __get(string $name) {
+    public function __get(string $name)
+    {
         if (!property_exists($this, $name)) {
             throw new \Exception(sprintf('%s property is not exists on %s',
                 $name, get_class($this)));
         }
         return $this->{$name};
+    }
+    public function __isset(string $name): bool
+    {
+        if (property_exists($this, $name)) {
+            return isset($this->{$name});
+        }
+        return false;
+    }
+    public function __unset(string $name)
+    {
+        $this->{$name} = null;
     }
 }
