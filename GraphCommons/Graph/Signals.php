50ab994b0d616b41adb3f1559997ed9a781062da
Kerem Gunes <qeremy@gmail.com>
2015-12-26 00:09:05 +0200

    add add() method instead using set().

--- GraphCommons/Graph/Signals.php
+++ GraphCommons/Graph/Signals.php
@@ -6,21 +6,8 @@ use GraphCommons\Graph\Signal;
 
 class Signals extends Collection
 {
-    final public function set($key, $value): self
+    final public function add(Signal $signal)
     {
-        if (!$value instanceof Signal) {
-            throw new \Exception(
-                'Entity must be instance of GraphCommons\Graph\Signal'
-            );
-        }
-        if ($key === null) {
-            $key = $this->count();
-        }
-        return parent::set($key, $value);
-    }
-    final public function get($key)
-    {
-
-        return parent::get($key);
+        return parent::set($this->count(), $signal);
     }
 }
