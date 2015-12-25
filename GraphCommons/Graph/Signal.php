96fd94a4cb8ab0a2aa9b4c60abb62d8b020369cf
Kerem Gunes <qeremy@gmail.com>
2015-12-26 00:04:48 +0200

    remove unneeded const prefixes.

--- GraphCommons/Graph/Signal.php
+++ GraphCommons/Graph/Signal.php
@@ -3,16 +3,16 @@ namespace GraphCommons\Graph;
 
 final class Signal
 {
-    const ACTION_NODE_CREATE     = 1,
-          ACTION_NODE_UPDATE     = 2,
-          ACTION_NODE_DELETE     = 3,
-          ACTION_EDGE_CREATE     = 4,
-          ACTION_EDGE_UPDATE     = 5,
-          ACTION_EDGE_DELETE     = 6,
-          ACTION_NODETYPE_UPDATE = 7,
-          ACTION_NODETYPE_DELETE = 8,
-          ACTION_EDGETYPE_UPDATE = 9,
-          ACTION_EDGETYPE_DELETE = 10;
+    const NODE_CREATE     = 1,
+          NODE_UPDATE     = 2,
+          NODE_DELETE     = 3,
+          EDGE_CREATE     = 4,
+          EDGE_UPDATE     = 5,
+          EDGE_DELETE     = 6,
+          NODETYPE_UPDATE = 7,
+          NODETYPE_DELETE = 8,
+          EDGETYPE_UPDATE = 9,
+          EDGETYPE_DELETE = 10;
     protected $action;
     protected $actions = array(
         1 => 'node_create',
