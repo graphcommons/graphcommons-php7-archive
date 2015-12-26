dce657716865c491564617276a837bfe94af34ef
Kerem Gunes <qeremy@gmail.com>
2015-12-26 02:11:47 +0200

    add getJsonError() method.

--- GraphCommons/Util/Util.php
+++ GraphCommons/Util/Util.php
@@ -100,4 +100,21 @@ abstract class Util
             'message' => GraphCommonsApiException::UNKNOWN_ERROR_MESSAGE,
         );
     }
+
+    final public static function getJsonError(int $code = null): string
+    {
+        static $errors = array(
+            JSON_ERROR_NONE           => '',
+            JSON_ERROR_DEPTH          => 'Maximum stack depth exceeded',
+            JSON_ERROR_STATE_MISMATCH => 'State mismatch (invalid or malformed JSON)',
+            JSON_ERROR_CTRL_CHAR      => 'Unexpected control character found',
+            JSON_ERROR_SYNTAX         => 'Syntax error, malformed JSON',
+            JSON_ERROR_UTF8           => 'Malformed UTF-8 characters, possibly incorrectly encoded'
+        );
+        if ($code === null) {
+            $code = json_error_code();
+        }
+        return array_key_exists($code, $errors)
+            ? $errors[$code] : "Unknown error ({$error})";
+    }
 }
