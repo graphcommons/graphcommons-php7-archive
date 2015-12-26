31944f331d475edd03ca3c354e215e1f562a19ce
Kerem Gunes <qeremy@gmail.com>
2015-12-26 04:08:12 +0200

    test new features.

--- test/test.php
+++ test/test.php
@@ -6,6 +6,7 @@ $autoload = require('../GraphCommons/Autoload.php');
 $autoload->register();
 
 use GraphCommons\GraphCommons;
+use GraphCommons\Graph\Graph;
 use GraphCommons\Graph\Signal;
 use GraphCommons\Graph\SignalCollection;
 
@@ -18,10 +19,51 @@ $gc = new GraphCommons(API_KEY);
 // get graph
 // $data = $gc->api->getGraph('8f8c794a-a498-4c3e-a73b-95a460db6e3a');
 
-// dump data
-// pre($data);
+// post graph
+$data = $gc->api->postGraph((function() {
+    $graph = new Graph();
+    $graph->setName('Birds');
+    $graph->setDescription('The Worl of Birds!');
+    $graph->setStatus(Graph::STATUS_DRAFT);
+    $signals = SignalCollection::fromArray(array(
+        array(
+            'action' => Signal::NODE_CREATE,
+            'parameters' => array(
+                'name' => 'The foo',
+                'type' => 'foo',
+            ),
+        ),
+        array(
+            'action' => Signal::NODE_CREATE,
+            'parameters' => array(
+                'from_name' => 'The foo',
+                'from_type' => 'foo',
+                'weight' => 2,
+            ),
+        ),
+    ));
+    return $graph->setSignals($signals);
+})());
 
-$ss = new SignalCollection();
-$ss->add(new Signal(Signal::NODE_CREATE));
-$ss->add(new Signal(Signal::NODE_UPDATE));
-pre($ss);
+// dump data
+pre($data);
+
+// pre(SignalCollection::fromArray(array(
+//     array(
+//         'action' => Signal::NODE_CREATE,
+//         'parameters' => array(
+//             'name' => 'The foo',
+//             'type' => 'foo',
+//         ),
+//     ),
+//     array(
+//         'action' => Signal::NODE_CREATE,
+//         'parameters' => array(
+//             'from_name' => 'The foo',
+//             'from_type' => 'foo',
+//             'weight' => 2,
+//         ),
+//     ),
+// )));
+
+// pre(SignalCollection::fromJson('{"signals":[{"action":"node_create","name":"The foo","type":"foo"},{"action":"node_create","from_name":"The foo","from_type":"foo","weight":2}]}'));
