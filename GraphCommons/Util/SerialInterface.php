9d41ee68c783bdb0693ae3166e7aad0825a761a6
Kerem Gunes <qeremy@gmail.com>
2015-12-26 22:02:13 +0200

    rename SerialInterface as SerialTrait.

--- GraphCommons/Util/SerialInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace GraphCommons\Util;
-
-interface SerialInterface
-{
-    public function serialize(...$args): string;
-    public function unserialize(...$args): array;
-}
---
+++ GraphCommons/Util/SerialTrait.php
@@ -0,0 +1,21 @@
+<?php
+namespace GraphCommons\Util;
+
+trait SerialTrait
+{
+    final public function serialize(...$args): string
+    {
+        $json = new Json($this->unserialize());
+        if ($json->hasError()) {
+            $jsonError = $json->getError();
+            throw new JsonException(sprintf(
+                'JSON error: code(%d) message(%s)',
+                $jsonError['code'], $jsonError['message']
+            ),  $jsonError['code']);
+        }
+
+        return (string) $json->encode();
+    }
+
+    abstract public function unserialize(...$args): array;
+}
