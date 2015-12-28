0a3e7a2dcce7bb1617a2b2f68e5d5d83b919d439
Kerem Gunes <qeremy@gmail.com>
2015-12-28 03:18:27 +0200

    add license & docs.

--- GraphCommons/Autoload.php
+++ GraphCommons/Autoload.php
@@ -1,25 +1,73 @@
 <?php
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Graph Commons & contributors.
+ *     <http://graphcommons.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 namespace GraphCommons;
 
+// add simply php version check
 if (PHP_VERSION[0] != '7') {
     throw new \RuntimeException('GraphCommons-PHP requires minimum PHP 7 version!');
 }
 
+/**
+ * @package GraphCommons
+ * @object  GraphCommons\Autoload
+ * @author  Kerem Güneş <qeremy@gmail.com>
+ */
 final class Autoload
 {
+    /**
+     * Singleton thing.
+     * @var GraphCommons\Autoload
+     */
     private static $instance;
 
+    /**
+     * Idle init stoppers.
+     */
     final private function __clone() {}
     final private function __construct() {}
 
+    /**
+     * Initializer.
+     *
+     * @return GraphCommons\Autoload
+     */
     final public static function init(): self
     {
         if (self::$instance == null) {
             self::$instance = new self();
         }
+
         return self::$instance;
     }
 
+    /**
+     * Registrar.
+     *
+     * @return void
+     */
     final public function register()
     {
         spl_autoload_register(function($name) {
